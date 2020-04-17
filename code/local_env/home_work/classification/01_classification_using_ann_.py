d2b8866dd816f72970638d47db050b6c7d22529e
balaji <balaji.chopparapu@gmail.com>
2020-04-17 11:10:42 +0530

    Added few more examples and work books

---
+++ code/local_env/home_work/classification/01_classification_using_ann_.py
@@ -0,0 +1,36 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+
+import pandas as pd
+# fix random seed for reproducibility - it allows that no matter if we execute
+# the code more than one time, the random values have to be the same
+# Importing the dataset
+dataset = pd.read_csv('../../data/Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# End of data preprocessing
+
+# create model
+model = Sequential()
+model.add(Dense(4, input_dim=2, activation="relu", kernel_initializer="uniform"))
+model.add(Dense(2, activation="relu", kernel_initializer="uniform"))
+model.add(Dense(1, activation="sigmoid", kernel_initializer="uniform"))
+
+# Compile model
+# binary_crossentropy = logarithmic loss
+# adam = gradient descent algorithm
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, y, epochs=150, batch_size=10)
+
+# Evaluating model with the training data
+scores = model.evaluate(X, y)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
---
+++ notes.md
@@ -0,0 +1,22 @@
+# environment setup
+    source ~/.local/bin/virtualenvwrapper.sh
+    source ~/.bashrc
+    mkvirtualenv dlwpb -p python3
+    workon dlwpb
+    #export TMPDIR=/mnt/disks/data/tmp
+    pip install numpy
+    pip install tensorflow==2.0.0 
+    pip install scikit-learn
+    pip install matplotlib
+    pip install spyder
+    pip install Theano
+    pip install keras
+    pip install pandas
+    pip install pillow
+# Data location
+     /mnt/disks/data/deep_learning_A-Z
+     
+# on google cloud machine
+    cd  /mnt/disks/data/deep_learning_A-Z
+    source /usr/local/bin/virtualenvwrapper.sh
+    source ~/.bashrc
